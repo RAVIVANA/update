1ac8b42d92f534bc2dce854526bb89850ce48ddb
RAVIVANA <136336583+RAVIVANA@users.noreply.github.com>
2023-06-22 19:51:28 +0530

    Add files via upload

---
+++ com/nkxgen/spring/jdbc/Dao/TransactionsInterface.java
@@ -0,0 +1,33 @@
+package com.nkxgen.spring.jdbc.Dao;
+
+import com.nkxgen.spring.jdbc.model.Account;
+import com.nkxgen.spring.jdbc.model.Customertrail;
+import com.nkxgen.spring.jdbc.model.LoanAccount;
+import com.nkxgen.spring.jdbc.model.Transaction;
+import com.nkxgen.spring.jdbc.model.transactioninfo;
+
+public interface TransactionsInterface {
+
+	public Account getAccountById(int id);
+
+	public LoanAccount getLoanAccountById(long acnt_id);
+
+	public void moneyDeposit(transactioninfo tempacc);
+
+	public void loanRepayment();
+
+	public void interestDeposit();
+
+	public void moneyWithdrawl(transactioninfo tempacc);
+
+	public void loanWithdrawl(long id);
+
+	public Transaction transactionSave(transactioninfo tarn);
+
+	public void saveTransaction(Transaction t);
+
+	public Transaction transactionSave1(transactioninfo tarn);
+
+	public Customertrail getCustomerByLoanID(Long loanId);
+
+}
