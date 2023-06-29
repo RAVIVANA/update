ebc1e3e02495bf5d5179d470d1e172786bed12c3
RAVIVANA <136336583+RAVIVANA@users.noreply.github.com>
2023-06-29 18:08:45 +0530

    Delete webapp directory

--- webapp/WEB-INF/js/cash-chest.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var dateTimeField = document.getElementById('dateTime');
-	var currentDate = new Date();
-	var dateString = currentDate.toLocaleDateString();
-	var timeString = currentDate.toLocaleTimeString();
-	dateTimeField.value = dateString + ' ' + timeString;
-});
